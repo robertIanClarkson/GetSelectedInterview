368fb9c1c132a0fcc24a19e637d12aabe26d0bea
Robert Clarkson <robert.ian.clarkson@gmail.com>
2021-06-21 00:36:18 -0700

    Created 'SubjectData.js' which holds mock Subject data. Data is in the for { name:str, courses:[str, ...] }.

---
+++ client/src/SubjectData.js
@@ -0,0 +1,22 @@
+
+
+const subjects = [
+  {
+    name: "English",
+    courses: [
+      "Journalism",
+      "Literacy",
+      "Poetry"
+    ]
+  },
+  {
+    name: "Math",
+    courses: [
+      "Algebra",
+      "Algebra II",
+      "Calculus"
+    ]
+  },
+];
+
+export default subjects;
