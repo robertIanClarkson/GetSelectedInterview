82b715beb71d91be5132923f9bd12a4bbc90bd3f
Robert Clarkson <robert.ian.clarkson@gmail.com>
2021-06-21 00:37:03 -0700

    Wrote a small, unoptimized prototype. Used vanilla React to test some concepts. Looks like hiding tabs in mobile might be an issue.

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './App.css';
 import {
   PageContainer,
@@ -15,6 +16,64 @@ import {
   Logo
 } from "./StyledComponents";
 
+import Select from 'react-select';
+import subjectData from './SubjectData';
+
+
+const Onboarding = () => {
+  const [subject, setSubject] = useState("")
+  const [courses, setCourses] = useState([])
+
+  let subjectOptions = [];
+  for(const subject of subjectData){
+    subjectOptions.push({
+      label: subject.name,
+      value: subject.courses
+    });
+  }
+
+  const handleSubjectChange = (e) => {
+    setSubject(e.label);
+  };
+
+  const handleCourseChange = (e) => {
+    let temp = []
+    for (const course of e) {
+      temp.push(course.value)
+    }
+    setCourses(temp)
+  };
+
+  let courseOptions = []
+  if(subject) {
+    for (const course of subjectData.find((sub) => sub.name === subject).courses) {
+      courseOptions.push({
+        value: course,
+        label: course
+      })
+    }
+  }
+  return (
+    <PanelContainer>
+      <PanelSection>
+        <PanelSectionTitle>
+          Subjects
+        </PanelSectionTitle>
+        <PanelMainColumn>
+          Select 2 subjects you're most qualified to teach. You can edit these or add more subjects later.
+          <Select options={subjectOptions} onChange={handleSubjectChange} />
+          {subject !== "" && <Select 
+            options={courseOptions} 
+            onChange={handleCourseChange}
+            isMulti/>}
+        </PanelMainColumn>
+      </PanelSection>
+      <button onClick={() => console.log(subject, courses)}>back</button>
+      <button onClick={() => console.log(subject, courses)}>submit</button>
+    </PanelContainer>
+  );
+};
+
 function App() {
   return (
     <PageContainer>
@@ -22,7 +81,7 @@ function App() {
         <Logo src="./logo.svg" />
       </PageNavBar>
       <PageContentContainer>
-        <PageTitle>Teacher Search</PageTitle>
+        <Onboarding />
       </PageContentContainer>
     </PageContainer>
   );
